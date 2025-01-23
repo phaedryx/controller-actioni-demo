f542e1e57cc1ce876e077ff7f7e8478a01a1c884
Tad Thorley <phaedryx@gmail.com>
2025-01-23 09:20:40 -0700

    default to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :set_current_attributes
+  skip_before_action :verify_authenticity_token
 
   def set_current_attributes
     Current.request_id = request.request_id
